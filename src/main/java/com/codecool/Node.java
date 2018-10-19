f55177450bc26436db253272768cd7a1f7666352
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 10:56:25 +0200

    Implement setters, getters

--- src/main/java/com/codecool/Node.java
+++ src/main/java/com/codecool/Node.java
@@ -5,8 +5,24 @@ public class Node {
     private Object obj;
     private Node nextNode;
 
-    public Node(Object obj, Node nextNode) {
+    Node(Object obj, Node nextNode) {
         this.obj = obj;
+        this.nextNode = nextNode;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
         this.nextNode = nextNode;
     }
 }
