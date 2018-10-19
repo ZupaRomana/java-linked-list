f74993de9b3ad3e9ad4e1b09a8c72d691c3f7b75
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 10:51:55 +0200

    Add fields, constructor

--- src/main/java/com/codecool/Node.java
+++ src/main/java/com/codecool/Node.java
@@ -1,4 +1,12 @@
 package com.codecool;
 
 public class Node {
+
+    private Object obj;
+    private Node nextNode;
+
+    public Node(Object obj, Node nextNode) {
+        this.obj = obj;
+        this.nextNode = nextNode;
+    }
 }
