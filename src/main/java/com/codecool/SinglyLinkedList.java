3a77c506cc6915bc5ea9c6ec3bd87ef7f41fb150
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 11:09:17 +0200

    Implement early version of class

--- src/main/java/com/codecool/SinglyLinkedList.java
+++ src/main/java/com/codecool/SinglyLinkedList.java
@@ -11,4 +11,19 @@ public class SinglyLinkedList {
         this.last = null;
         this.length = 0;
     }
+
+    public void add(Object obj) {
+        if (this.length == 0) {
+            this.head = new Node(obj, null);
+            this.last = this.head;
+        } else if (this.length == 1) {
+            this.last = new Node(obj, null);
+            this.head.setNextNode(this.last);
+        } else {
+            Node tempNode = this.last;
+            this.last = new Node(obj, null);
+            tempNode.setNextNode(this.last);
+        }
+        length++;
+    }
 }
