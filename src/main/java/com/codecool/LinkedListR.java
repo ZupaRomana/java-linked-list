a44baace714631f87525432f379aafaa4b4c1998
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 10:39:41 +0200

    Implement fields and constructors

--- src/main/java/com/codecool/LinkedListR.java
+++ src/main/java/com/codecool/LinkedListR.java
@@ -1,4 +1,15 @@
 package com.codecool;
 
 public class LinkedListR {
+
+    private static final int STARTING_STANDARD_ARRAY_SIZE = 0;
+    private Integer[] array;
+
+    public LinkedListR() {
+        this.array = new Integer[STARTING_STANDARD_ARRAY_SIZE];
+    }
+
+    public LinkedListR(int size) {
+        this.array = new Integer[size];
+    }
 }
