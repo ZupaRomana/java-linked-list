7028ae9b0d63f388f4f7d31cfcfe25c3c6c23b82
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 11:16:40 +0200

    Add test checking simply adding object to list

--- src/test/java/com/codecool/SinglyLinkedListTest.java
+++ src/test/java/com/codecool/SinglyLinkedListTest.java
@@ -1,6 +1,20 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
 public class SinglyLinkedListTest {
 
+    private SinglyLinkedList linkedList = new SinglyLinkedList();
+
+    @Test
+    void add_simple_object() {
+        this.fillList(1);
+    }
+
+    private void fillList(int numOfObjects) {
+        for (int i = 0; i <= numOfObjects; i++) {
+            this.linkedList.add(i);
+        }
+    }
 
 }
