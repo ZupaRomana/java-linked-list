4a8440a8e9a2a1990a0f1c4f536b964fe9209db7
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-19 12:16:45 +0200

    Change linked list to be generic type, Delete Node class

--- src/main/java/com/codecool/SinglyLinkedList.java
+++ src/main/java/com/codecool/SinglyLinkedList.java
@@ -1,9 +1,9 @@
 package com.codecool;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<Element> {
 
-    private Node head;
-    private Node last;
+    private Node<Element> head;
+    private Node<Element> last;
     private int length;
 
     public SinglyLinkedList() {
@@ -12,22 +12,48 @@ public class SinglyLinkedList {
         this.length = 0;
     }
 
-    public void add(String element) {
+    public void add(Element element) {
         if (this.length == 0) {
-            this.head = new Node(element);
+            this.head = new Node<>(element);
             this.last = this.head;
         } else if (this.length == 1) {
-            this.last = new Node(element);
+            this.last = new Node<>(element);
             this.head.setNextNode(this.last);
         } else {
-            Node tempNode = this.last;
-            this.last = new Node(element);
+            Node<Element> tempNode = this.last;
+            this.last = new Node<>(element);
             tempNode.setNextNode(this.last);
         }
         length++;
     }
 
-    public String getHead() {
-        return this.head.getObj();
+    public Element getHead() {
+        return this.head.getElement();
+    }
+
+    class Node<Element> {
+        private Element element;
+        private Node<Element> nextNode;
+
+        Node(Element element) {
+            this.element = element;
+            this.nextNode = null;
+        }
+
+        Element getElement() {
+            return element;
+        }
+
+        void setElement(Element element) {
+            this.element = element;
+        }
+
+        Node<Element> getNextNode() {
+            return this.nextNode;
+        }
+
+        void setNextNode(Node<Element> nextNode) {
+            this.nextNode = nextNode;
+        }
     }
 }
