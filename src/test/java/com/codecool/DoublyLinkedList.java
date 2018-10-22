4837f8fa18ceade5577a8700c9b81ae3e2fc95ec
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-22 14:23:27 +0200

    Implement inner class extending inner class that is in extended class

--- src/test/java/com/codecool/DoublyLinkedList.java
+++ src/test/java/com/codecool/DoublyLinkedList.java
@@ -2,4 +2,16 @@ package com.codecool;
 
 public class DoublyLinkedList<Element> extends SinglyLinkedList<Element> {
 
+    public DoublyLinkedList() {
+        super();
+    }
+
+    class NodeDouble<E> extends Node<E> {
+        private NodeDouble<E> previousNode;
+
+        NodeDouble(E nextElement) {
+            super(nextElement);
+            this.previousNode = null;
+        }
+    }
 }
