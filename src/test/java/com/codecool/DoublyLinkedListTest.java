aeb6012380ed70b78743451fcdfa952c9467a882
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-22 16:31:08 +0200

    Add test checking adding obj

--- src/test/java/com/codecool/DoublyLinkedListTest.java
+++ src/test/java/com/codecool/DoublyLinkedListTest.java
@@ -1,7 +1,16 @@
 package com.codecool;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedListTest {
+
+    private DoublyLinkedList<String> list = new DoublyLinkedList<>();
+
+    @Test
+    void addsObject_when_oneObjectPassed() {
+        list.add("example");
+    }
 
 }
