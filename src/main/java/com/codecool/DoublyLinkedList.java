b1f3c21b4eae2696dfe24d7f24b9f0afbf7e60c0
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-25 14:15:46 +0200

    Implement remove() method

--- src/main/java/com/codecool/DoublyLinkedList.java
+++ src/main/java/com/codecool/DoublyLinkedList.java
@@ -75,7 +75,12 @@ public class DoublyLinkedList<Element> {
         if (badIndexPassed(index)) {
             throw new ArrayIndexOutOfBoundsException();
         } else {
-
+            Node<Element> focus = head;
+            for (int i = 1; i < index; i++) {
+                focus = focus.nextNode;
+            }
+            focus.previousNode.nextNode = focus.nextNode;
+            focus.nextNode.previousNode = focus.previousNode;
         }
     }
 
